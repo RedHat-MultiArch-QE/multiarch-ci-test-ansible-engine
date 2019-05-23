765da3858a44c92267e24dc3bc6ee25839583e21
Jeremy Poulin <jpoulin@redhat.com>
2019-05-23 15:59:17 -0400

    Install missing beaker lib and formatted test better.

--- tests/scripts/rhel-system-roles/test.sh
+++ tests/scripts/rhel-system-roles/test.sh
@@ -2,14 +2,26 @@
 #
 # Attempts to clone down the test package and run it
 
+# Save directory info
 cd "$(dirname ${BASH_SOURCE[0]})"
 workdir=$(pwd)
+
+# Install dependencies
+sudo yum install beakerlib python3-lxml
+
+# Clone test
 rhpkg --verbose --user=jenkins clone tests/rhel-system-roles
 cd rhel-system-roles
 git checkout private-upstream_testsuite_refactor
 cd Sanity/Upstream-testsuite
+
+# Define output
 output_dir="$workdir/artifacts/rhel-system-roles/results"
 output_file="$output_dir/$(arch)-test-output.txt"
 mkdir -p $output_dir
+
+# Run the test
 sudo make &> $output_file run
+
+# Ensure Success
 grep "OVERALL RESULT" $output_file | grep "PASS"
